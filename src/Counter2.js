e94b9a0fc1b665b2615ecd9f324c8b0e655689e0
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-04-13 18:10:28 +0530

    Use Reducer

---
+++ src/Counter2.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+// Actions
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const RESET = "RESET";
+
+const countReducer = (count, action) => {
+  switch (action.type) {
+    case INCREMENT: {
+      return count + action.by;
+    }
+    case DECREMENT: {
+      return count - action.by;
+    }
+    case RESET: {
+      return action.by;
+    }
+    default: {
+      return 0;
+    }
+  }
+};
+
+function Counter2() {
+  const [count, dispatch] = React.useReducer(countReducer, 0);
+
+  return (
+    <>
+      <p>Count : {count}</p>
+      <button onClick={() => dispatch({ type: INCREMENT, by: 2 })}>
+        Increment
+      </button>
+      <button onClick={() => dispatch({ type: DECREMENT, by: 4 })}>
+        Decrement
+      </button>
+      <button onClick={() => dispatch({ type: RESET })}>Reset</button>
+    </>
+  );
+}
+export default Counter2;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Counter from "./Counter";
+import Counter2 from "./Counter2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Counter />);
+root.render(<Counter2 />);
